37e5d16d48bcb4eb240f234f7ea45dce4d242387
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-03-24 19:12:25 -0700

    Challenge day 1

--- Day 01/Q3 Name cases/app.ts
+++ Day 01/Q3 Name cases/app.ts
@@ -1,3 +1,5 @@
+//Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
 // lower case
 let personName: string ="Bilal";
 console.log("lowercase:", personName.toLowerCase());
