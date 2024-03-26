297e4a90d71e4782a9fb2d4d8cfc60f72498259f
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-03-26 09:35:04 -0700

    Challenge Day 03

---
+++ Day 03/Q7,8 number eight/app.ts
@@ -0,0 +1,8 @@
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+//Question 8: You should create four lines that look like this:
+//console.log(5 + 3)
+
+console.log(5+3);
+console.log(11-3);
+console.log(2*4);
+console.log(16/2);
