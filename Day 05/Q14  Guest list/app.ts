bf9c637623fda94c5c8cdf6c9a5495d86be7a42a
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-04-16 12:31:12 -0700

    Day 05

---
+++ Day 05/Q14  Guest list/app.ts
@@ -0,0 +1,10 @@
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+// Guest List
+
+let guestList: string[] = [`Zenat`, `Saqib`, `Baba` , `Jiya`];
+
+// Invite each guest to dinner
+
+for (let guest of guestList) {
+    console.log(`\n\nAssalam o Alikum Sir/Madam ${guest}, \nWe want to invited u on dinner.\n\nThank You`);
+}
---
+++ Day 05/Q15 Changing guest list/app.ts
@@ -0,0 +1,23 @@
+// Changing Guest List: One of your guests can't make it to the dinner, so you need to send out a new set of invitations with a replacement guest.
+// Initial guest list
+let guestList: string[] = [`Zenat`, `Saqib` , `Baba` , `Jiya`];
+
+// Function to print invitation message
+function printInvitation(guest: string) {
+    console.log(`\nAsslam o Alikum Sir/Madam  ${guest},\nYou are invited to dinner. Please join us!\nThank You\n\n`);
+}
+// Invite initial guests
+console.log("\nInitial Guest List:\n");
+for (let guest of guestList) {
+    printInvitation(guest);
+}
+// Modify guest list
+guestList[3] = "Ayesha "; 
+// Invite updated guests
+console.log("\nUpdated Guest List:\n");
+for (let guest of guestList) {
+    printInvitation(guest);
+}
+// Identify guest who can't make it
+let unableToAttend = guestList[3]; 
+console.log(`Sir/Madam ${unableToAttend} can't make it to the dinner.`);
