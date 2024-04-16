7041524f7b008c5d7970d4e42743d3841a7c3231
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-04-16 13:24:24 -0700

    Day 08

---
+++ Day 08/Q22 intentional error/app.ts
@@ -0,0 +1,10 @@
+// Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+
+let fruits: string[] = ["apple", "banana", "orange"];
+
+console.log(`\nValid Fruit Nmae:\n `) 
+// Trying to access an index that doesn't exist
+console.log(fruits[3]); 
+
+// Correcting the error by access a valid index
+console.log(fruits[2]); 
