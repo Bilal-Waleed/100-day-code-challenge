07bce893599202f279c198d2c0709c7b8e6235e7
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-04-18 07:15:45 -0700

    Day 11

---
+++ Day 11/Q31 No Users/app.ts
@@ -0,0 +1,10 @@
+// No Users: Ensure your user list isn’t empty.
+
+let usernames: string[] = []; // Empty array
+
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    
+}
+// removing all username ensure the message 
---
+++ Day 11/Q32 Checking Username/app.ts
@@ -0,0 +1,21 @@
+// Checking Usernames: Ensure uniqueness in usernames.
+
+let current_users: string[] = ['john', 'mary', 'alex', 'sarah', 'david'];
+let new_users: string[] = ['John', 'jane', 'alex', 'mike', 'emily'];
+
+for (let new_user of new_users) {
+    let usernameExists: boolean = false;
+
+    for (let current_user of current_users) {
+        if (new_user.toLowerCase() === current_user.toLowerCase()) {
+            usernameExists = true;
+            break;
+        }
+    }
+    
+    if (usernameExists) {
+        console.log(`Sorry, the username '${new_user}' is already taken. Please enter a new username.`);
+    } else {
+        console.log(`Congratulations! The username '${new_user}' is available.`);
+    }
+}
