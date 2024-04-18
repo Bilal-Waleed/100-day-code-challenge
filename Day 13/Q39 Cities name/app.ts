961b6c26fe5030b6304b3ab7961e9a86b4931aad
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-04-18 08:49:08 -0700

    Day 13

---
+++ Day 13/Q39 Cities name/app.ts
@@ -0,0 +1,14 @@
+// City Names: Formatting city-country pairs.
+
+function city_country(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+
+// Call the function with at least three city-country pairs
+let city1: string = city_country('Lahore', 'Pakistan');
+let city2: string = city_country('Paris', 'France');
+let city3: string = city_country('New York', 'USA');
+
+console.log(city1);
+console.log(city2);
+console.log(city3);
