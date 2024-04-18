28fdc310893e932a6c9a35d2fe13432fa1ce18da
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-04-18 07:29:33 -0700

    Day 12

---
+++ Day 12/Q34 Pizzas/app.ts
@@ -0,0 +1,9 @@
+// Pizzas: Share your favorite pizzas and express your love for them.
+
+let favorite_pizzas: string[] = ['Pepperoni', 'Margherita', 'Supreme'];
+
+for (let pizza of favorite_pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("I really love pizza!");
---
+++ Day 12/Q36 T-Shirt/app.ts
@@ -0,0 +1,8 @@
+// T-Shirt: Create a function for customizing t-shirts.
+
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size.toUpperCase()} and the message printed on it is: "${message}"`);
+}
+
+// Call the function
+make_shirt('medium', 'Keep Calm and Code On');
